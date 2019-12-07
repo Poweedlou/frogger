f6ab1f24a5a521ca7643a7c8dd3b51e3bb647597
Poweedlou <balalin2003@gmail.com>
2019-12-07 21:33:02 +0300

    impoved

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from pygame import Color, Rect
+import pygame
 import pygame.display
 import pygame.image as img
 import pygame.time as time
-from pygame.transform import rotate
+from pygame.transform import rotate, flip
 
 
 class Field:
@@ -14,37 +15,35 @@ class Field:
         chicken.parent = self
         self.ch_coords = [width // 2, 4]
         self.lines = []
-        self.seen_lines = []
+        self.seen_lines = 0
         self.playing = False
-        self.cam_y = 4
+        self.cam_y = 0
         self.ch_max_y = 4
-        self.max_cam_y = 4
-        self.cam_y_real = 4
-        self.cam_moves = False
-        self.screen = pygame.display.set_mode((width * cell_size, height * cell_size))
+        self.cam_y_real = 0
+        x = width * cell_size
+        y = height * cell_size
+        self.screen = pygame.display.set_mode((x, y))
+        self.screen2 = pygame.Surface((x, y + cell_size))
+        self.screen_size = (x, y)
 
-    def play(self):
-        if self.playing:
-            self.lines.clear()
-        self.playing = not self.playing
-    
+
     def frame(self, force=False):
         if self.playing or force:
             y = self.ch_coords[1]
             self.cam_y_real += 0.02
-            if y > self.cam_y:
-                self.cam_y += (y - self.cam_y) * 0.05
+            if y > self.cam_y + 4:
+                self.cam_y += (y - self.cam_y - 4) * 0.05
             if self.cam_y_real > self.cam_y:
                 self.cam_y = self.cam_y_real
             new_lines = []
             for line in self.lines:
-                if line.y >= self.cam_y - 4:
+                if line.y > self.cam_y - 1:
                     new_lines.append(line)
-            for i in range(int(self.cam_y) - 4, int(self.cam_y) - 3 + self.height):
-                if i not in self.seen_lines:
-                    line = self.gen_line(i)
-                    new_lines.append(line)
-                    self.seen_lines.append(i)
+            upper_border = int(self.cam_y) + self.height + 1
+            for i in range(self.seen_lines, upper_border):
+                line = self.gen_line(i)
+                new_lines.append(line)
+            self.seen_lines = upper_border
             self.lines = new_lines
             self.render()
 
@@ -65,44 +64,42 @@ class Field:
     
     def gen_line(self, y):
         bg = Color(['white', 'blue'][y % 2])
-        return Line(y, self, bg)
+        return Line(y, bg, self.width, self.cell_size)
 
     def render(self):
-        self.screen.fill((255, 255, 255))
-        for i in self.lines:
-            i.render()
-        self.chicken.render()
+        shift = int(self.cam_y)
+        lines = map(Line.render, self.lines)
+        lines = map(lambda x: (x[0], (0, (x[1] - shift) * self.cell_size)), lines)
+        self.screen2.blits(lines)
+        ch_x, ch_y = self.ch_coords
+        ch_y -= shift
+        ch_x *= self.cell_size
+        ch_y *= self.cell_size
+        self.screen2.blit(self.chicken.sprite, (ch_x, ch_y))
+        foo = flip(self.screen2, False, True)
+        y_blit = self.cell_size * (1 - self.cam_y + shift)
+        blit_area = Rect((0, y_blit), self.screen_size)
+        self.screen.blit(foo, (0, 0), area=blit_area)
 
 
 class Line:
-    def __init__(self, y, parent, color):
+    def __init__(self, y, color, width, cell_size):
         self.y = y
-        self.color = color
-        self.parent = parent
+        size = (width * cell_size, cell_size)
+        self.screen = pygame.Surface(size)
+        self.screen.fill(color)
     
     def render(self):
-        scr = self.parent.screen
-        width = self.parent.width
-        cell = self.parent.cell_size
-        cam_y = self.parent.cam_y - 4
-        y = (self.parent.height - self.y - 1 + cam_y) * cell
-        rect_ = Rect(0, y, width * cell, cell)
-        scr.fill(self.color, rect_)
+        return self.screen, self.y
 
 
 class Chicken:
-    def __init__(self, pic_path, ):
+    def __init__(self, pic_path):
         self.sprite = img.load('sprites/chicken/' + pic_path + '.png')
         self.angle = 0
     
-    def render(self):
-        x, y = self.parent.ch_coords
-        x *= self.parent.cell_size
-        y = (self.parent.height - 5 - y + self.parent.cam_y) * self.parent.cell_size
-        self.parent.screen.blit(self.sprite, (x, y))
-    
     def turn(self, angle):
-        rotate(self.sprite, angle - self.angle)
+        self.sprite = rotate(self.sprite, angle - self.angle)
         self.angle = angle
 
 if __name__ == "__main__":
